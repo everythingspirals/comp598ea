4e1cdeb0eb81b6f1528a364e4507c7c958d6b57a
Dushan Perera <everythingspirals>
2018-04-05 17:17:17 -0700

    push

--- wheels.py
+++ wheels.py
@@ -30,8 +30,8 @@ def stop():
     l_side.stop(), r_side.stop()
 
 def move_wheels(l_speed, r_speed, duration=0):
-    #l_side.run_forever(speed_sp=l_speed),
-    #r_side.run_forever(speed_sp=r_speed)
+    l_side.run_forever(speed_sp=l_speed),
+    r_side.run_forever(speed_sp=r_speed)
     #time.sleep(2.0)
     wait(duration)
     #stop()
